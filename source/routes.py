83af74220015de3f0370f110a188725eaf8436d2
Kollego <kollego102@yandex.ru>
2021-04-22 03:24:34 +0300

    Created videos tab

--- source/routes.py
+++ source/routes.py
@@ -8,3 +8,18 @@ from source import app
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
+
+
+@app.route('/home', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+
+@app.route('/videos', methods=['GET'])
+def videos():
+    return render_template('videos.html')
+
+
+@app.route('/vod-highlights', methods=['GET'])
+def vod_highlights():
+    return 'hello'
---
+++ source/static/videos-script.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("video-add-modal");
+
+var btn = document.getElementById("video-add-btn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
